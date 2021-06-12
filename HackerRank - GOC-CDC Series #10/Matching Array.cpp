19fcfb6c82d0c7c6c3bde36737cc1c236397884f
Aryan Agarwal <aryan.cs.kgp@gmail.com>
2021-06-13 00:36:18 +0530

    commit from bash at 13/06/2021 00:36:18

--- HackerRank - GOC-CDC Series #10/Matching Array.cpp
+++ HackerRank - GOC-CDC Series #10/Matching Array.cpp
@@ -4,94 +4,42 @@
 	srcPath : /home/aryan/Documents/cp/Matching_Array.cpp
 	url : https://www.hackerrank.com/contests/goc-cdc-series-10/challenges/matching-array
 */
-// C++ program for the above approach
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-// Function that counts the minimum
-// moves required to covert arr[] to brr[]
-int minOperations(int arr1[], int arr2[], int i, int j,
-				int n, map<pair<int, int>, int> table)
-{
-	
-	// Base Cases
-	pair<int, int> key = {i, j};
-	
-	if (arr1 == arr2)
-		return 0;
-	if (i >= n || j >= n)
-		return 0;
-
-	// If the result is already stored
-	if (table.find(key) != table.end())
-		return table[key];
-
-	// Compute the result
-	if (arr1[i] < arr2[j])
-		return 1 + minOperations(
-			arr1, arr2, i + 1, j + 1, n, table);
-
-	// Store the result
-	table[key] = max(
-		minOperations(arr1, arr2, i, j + 1, n, table),
-		minOperations(arr1, arr2, i + 1, j, n, table));
-
-	// Return the result
-	return table[key];
-}
-
-// Function that counts the minimum
-// moves required to sort the array
-void minOperationsUtil(int arr[], int n)
-{
-	int brr[n];
-
-	for(int i = 0; i < n; i++)
-		brr[i] = arr[i];
-
-	sort(brr, brr + n);
-
-	if (brr == arr)
-		cout << 0;
-
-	else
-	{
-		map<pair<int, int>, int> table;
-		
-		cout << n - minOperations(
-			brr, arr, 0, 0, n, table);
-	}
-}
-
-// Driver code
+#define ll long long
+ll pos[1000005];
+ll ma[1000005];
 int main()
 {
-	int n;
+    ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+    ll n;
     cin>>n;
-
-    vector <int> a(n),pos(n+1);
-    for(int i=0;i<n;i++)
+    ll a[n];
+    ll b[n];
+    for(ll i=0;i<n;i++)
     {
         cin>>a[i];
-        pos[a[i]]=i;
     }
-
-    int b[n];
-
-    for(int i=0;i<n;i++)
+    for(ll i=0;i<n;i++)
     {
         cin>>b[i];
-        b[i]=pos[b[i]];
     }
-
-    for(int i=0;i<n;i++)
+    for (ll i = 0; i < n; i++)
     {
-        cout<<b[i]<<" ";
+        pos[a[i]] = i+1;
     }
-    cout<<"\n";
+    vector <ll> v;
+    for (ll i = 0; i < n; i++)
+    {
+        v.push_back(pos[b[i]]);
+    }
 
-	
-	minOperationsUtil(b, n);
+
+    ll ans = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        ma[v[i]] = ma[v[i]-1]+1;
+        ans = max(ans, ma[v[i]]);
+    }
+    cout<< n - ans;
 }
-
-// This code is contributed by grand_master
