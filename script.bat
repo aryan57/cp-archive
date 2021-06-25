98dc09d152eb7b26899036cfb8397f0c594e3d89
aryan57 <aryan.cs.kgp@gmail.com>
2021-06-25 23:51:27 +0530

    commit from bash at 2021-06-25 23:51:27.120

---
+++ script.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set DIR="C:\Users\aryan\.cp-archive"
+for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set DATE=%%j
+set DATE=%DATE:~0,4%-%DATE:~4,2%-%DATE:~6,2% %DATE:~8,2%:%DATE:~10,2%:%DATE:~12,6%
+
+cd %DIR%
+call git add .
+call git commit -m "commit from bash at %DATE%"
+call git push
--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#DIR="/home/aryan/.cp-archive"
-#cd ${DIR}
-#git add .
-#git commit -m "commit from bash at ""$(date +'%d/%m/%Y %H:%M:%S')"
-#git push
+DIR="/home/aryan/.cp-archive"
+cd ${DIR}
+git add .
+git commit -m "commit from bash at ""$(date +'%d/%m/%Y %H:%M:%S')"
+git push
