688b9502749a5f3c91ff27e0c7217f006493ff13
aryan57 <aryan.cs.kgp@gmail.com>
2021-06-26 00:06:58 +0530

    2021-06-26 00:06:58

--- script.bat
+++ script.bat
@@ -2,9 +2,9 @@
 
 set DIR="C:\Users\aryan\.cp-archive"
 for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set DATE=%%j
-set DATE=%DATE:~0,4%-%DATE:~4,2%-%DATE:~6,2% %DATE:~8,2%:%DATE:~10,2%:%DATE:~12,6%
+set DATE=%DATE:~0,4%-%DATE:~4,2%-%DATE:~6,2% %DATE:~8,2%:%DATE:~10,2%:%DATE:~12,2%
 
 cd %DIR%
 call git add .
-call git commit -m "commit from batch at %DATE%"
+call git commit -m "%DATE%"
 call git push
