5bb9539860ec397f26085256db5f220612407e0b
aryan57 <aryan.cs.kgp@gmail.com>
2021-06-26 23:44:31 +0530

    2021-06-26 23:44:31

---
+++ local/False_Number.cpp
@@ -0,0 +1,92 @@
+/*
+	group : local
+	name : False_Number.cpp
+	srcPath : c:\Users\aryan\Desktop\cp\False_Number.cpp
+	url : c:\Users\aryan\Desktop\cp\False_Number.cpp
+*/
+/*
+    author : aryan57
+    created : 26-June-2021 19:32:03 IST
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifndef ONLINE_JUDGE
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define int long long
+#define X first
+#define Y second
+#define pb push_back
+#define sz(a) ((int)(a).size())
+#define all(a) (a).begin(), (a).end()
+#define F(i, a, b) for (int i = a; i <= b; i++)
+#define RF(i, a, b) for (int i = a; i >= b; i--)
+
+const int mxn = 1e5;
+const long long INF = 2e18;
+const int32_t M = 1000000007;
+// const int32_t M = 998244353;
+const long double pie = acos(-1);
+
+void solve_LOL()
+{
+    string s;
+    cin>>s;
+
+    string s1="";
+    s1+=s[0];
+    s1+='0';
+    string s2="";
+    s2+='1';
+    s2+=s[0];
+    // dbg(s1,s2);
+    if(s1<s2)
+    {
+        cout<<s1<<s.substr(1,sz(s)-1);
+    }
+    else
+    {
+        cout<<s2<<s.substr(1,sz(s)-1);
+
+    }
+
+    cout<<"\n";
+
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+// freopen("input.txt","r",stdin);
+// freopen("output.txt","w",stdout);
+#endif
+#ifdef ARYAN_SIEVE
+    sieve();
+#endif
+#ifdef ARYAN_SEG_SIEVE
+    segmented_sieve();
+#endif
+#ifdef ARYAN_FACT
+    fact_init();
+#endif
+    // cout<<fixed<<setprecision(10);
+    int _t=1;
+    cin>>_t;
+    for (int i=1;i<=_t;i++)
+    {
+        // cout<<"Case #"<<i<<": ";
+        solve_LOL();
+    }
+    return 0;
+}
