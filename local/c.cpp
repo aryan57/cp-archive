4da615932fdcb6db0daa2bc4e9a98f2268618baf
Aryan Agarwal <aryan.cs.kgp@gmail.com>
2021-07-01 20:39:09 +0530

    2021-07-01 20:39:09

--- local/c.cpp
+++ local/c.cpp
@@ -4,10 +4,6 @@
 	srcPath : /home/aryan/Documents/cp/c.cpp
 	url : /home/aryan/Documents/cp/c.cpp
 */
-/*
-    author : aryan57
-    created : 01-July-2021 16:23:43 IST
-*/
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -21,51 +17,12 @@ template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr
 #define dbg(...)
 #endif
 
-#define int long long
-#define X first
-#define Y second
-#define pb push_back
-#define sz(a) ((int)(a).size())
-#define all(a) (a).begin(), (a).end()
-#define F(i, a, b) for (int i = a; i <= b; i++)
-#define RF(i, a, b) for (int i = a; i >= b; i--)
-
-const int mxn = 1e5;
-const long long INF = 2e18;
-const int32_t M = 1000000007;
-// const int32_t M = 998244353;
-const long double pie = acos(-1);
-
-void solve_LOG()
+int main()
 {
-    // cqw
-}
-
-signed main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-#ifndef ONLINE_JUDGE
-// freopen("input.txt","r",stdin);
-// freopen("output.txt","w",stdout);
-#endif
-#ifdef ARYAN_SIEVE
-    sieve();
-#endif
-#ifdef ARYAN_SEG_SIEVE
-    segmented_sieve();
-#endif
-#ifdef ARYAN_FACT
-    fact_init();
-#endif
-    // cout<<fixed<<setprecision(10);
-    int _t=1;
-    // cin>>_t;
-    for (int i=1;i<=_t;i++)
-    {
-        // cout<<"Case #"<<i<<": ";
-        solve_LOG();
-    }
+    int x;
+    x=10;
+    
+    dbg(x);
+    
     return 0;
 }
