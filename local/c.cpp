49971a8231dd22aef6888a61ce9a5894aae1d181
aryan57 <aryan.cs.kgp@gmail.com>
2021-07-01 15:09:07 +0530

    2021-07-01 15:09:07

--- local/c.cpp
+++ local/c.cpp
@@ -6,7 +6,7 @@
 */
 /*
     author : aryan57
-    created : 28-June-2021 02:13:39 IST
+    created : 01-July-2021 14:24:56 IST
 */
 #include <bits/stdc++.h>
 using namespace std;
@@ -36,45 +36,132 @@ const int32_t M = 1000000007;
 // const int32_t M = 998244353;
 const long double pie = acos(-1);
 
+struct Edge
+{
+    int u,v,w;
+};
+
+/* 
+    dsu d(int n)
+    // It creates an undirected graph with n vertices and 0 edges.
+    // 0<=n<=1e8
+    // O(n)
+
+    int d.merge(int a, int b)
+    // It adds an edge (a, b)
+    // If the vertices a and b were in the same connected component, it returns the representative of this connected component. Otherwise, it returns the representative of the new connected component.
+    // 0<=a<n   0<=b<n
+    // O(alpha(n)) amortized
+
+    bool d.same(int a, int b)
+    // Returns whether the vertices a and b are in the same connected component
+    // 0<=a<n   0<=b<n
+    // O(alpha(n)) amortized
+
+    int d.leader(int a)
+    // Returns the representative of the connected component that contains the vertex a
+    // 0<=a<n
+    // O(alpha(n)) amortized
+
+    int d.size(int a)
+    // Returns the size of the connected component that contains the vertex a.
+    // 0<=a<n
+    // O(alpha(n)) amortized
+
+    vector<vector<int>> d.groups()
+    // Returns the list of the "list of the vertices in a connected component"
+    // O(n)
+*/
+struct dsu {
+  public:
+    dsu() : _n(0) {}
+    dsu(int n) : _n(n), parent_or_size(n, -1) {}
+
+    int merge(int a, int b) {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        int x = leader(a), y = leader(b);
+        if (x == y) return x;
+        if (-parent_or_size[x] < -parent_or_size[y]) std::swap(x, y);
+        parent_or_size[x] += parent_or_size[y];
+        parent_or_size[y] = x;
+        return x;
+    }
+
+    bool same(int a, int b) {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        return leader(a) == leader(b);
+    }
+
+    int leader(int a) {
+        assert(0 <= a && a < _n);
+        if (parent_or_size[a] < 0) return a;
+        return parent_or_size[a] = leader(parent_or_size[a]);
+    }
+
+    int size(int a) {
+        assert(0 <= a && a < _n);
+        return -parent_or_size[leader(a)];
+    }
+
+    std::vector<std::vector<int>> groups() {
+        std::vector<int> leader_buf(_n), group_size(_n);
+        for (int i = 0; i < _n; i++) {
+            leader_buf[i] = leader(i);
+            group_size[leader_buf[i]]++;
+        }
+        std::vector<std::vector<int>> result(_n);
+        for (int i = 0; i < _n; i++) {
+            result[i].reserve(group_size[i]);
+        }
+        for (int i = 0; i < _n; i++) {
+            result[leader_buf[i]].push_back(i);
+        }
+        result.erase(
+            std::remove_if(result.begin(), result.end(),
+                           [&](const std::vector<int>& v) { return v.empty(); }),
+            result.end());
+        return result;
+    }
+
+  private:
+    int _n;
+    // root node: -1 * component size
+    // otherwise: parent
+    std::vector<int> parent_or_size;
+};
+
 void solve_LOG()
 {
-    
-
-    // array<array<array<int,10>,10>,10> dp;
-    
-    // int n = dp.size();
-    // int m = dp[0].size();
-    // int o = dp[0][0].size();
-
-    // F(i,0,n-1)
-    // {
-    //     F(j,0,m-1)
-    //     {
-    //         F(k,0,o-1)
-    //         {
-    //             cout<<dp[i][j][k]<<" ";
-    //         }
-    //     }
-    // }
-
-    int n=10;
-    int m=10;
-    int o=10;
-
-    int g[n][m][o];
-
-    fill_n(&g[0][0][0],n*m*o,INF);
-
-    F(i,0,n-1)
-    {
-        F(j,0,m-1)
-        {
-            F(k,0,o-1)
-            {
-                cout<<g[i][j][k]<<" ";
-            }
-        }
+    int n,m;
+    cin>>n>>m;
+    dsu d(n+1);
+    vector <pair <int,int> > adj[n+1];
+    F(i,0,m-1)
+    {   
+        int u,v,w;
+        cin>>u>>v>>w;
+        d.merge(u,v);
+        adj[u].push_back({v,w});
+        adj[v].push_back({u,w});
     }
+
+    if(!d.same(1,n))
+    {
+        cout<<"NOT POSSIBLE\n";
+    }
+
+    int ans=INF;
+
+    for(auto p : adj[n])
+    {
+        int w=p.Y;
+        ans=min(ans,w);
+    }
+
+    cout<<ans;
+    
 
 }
 
