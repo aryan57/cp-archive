19417468ad047d14ea9385252d80baf306d6f0f6
Aryan Agarwal <aryan.cs.kgp@gmail.com>
2021-07-25 00:25:27 +0530

    2021-07-25 00:25:27

--- local/c.cpp
+++ local/c.cpp
@@ -4,19 +4,21 @@
 	srcPath : /home/aryan/cp-gedit/c.cpp
 	url : /home/aryan/cp-gedit/c.cpp
 */
+/**
+ *    author:  Aryan Agarwal
+ *    created: 24.07.2021 23:54:38 IST       
+**/
 #include <bits/stdc++.h>
 using namespace std;
 const int32_t M = 1e9 + 7;
-const int32_t inf = 1e9;
 
 #define int long long
+const int INF = 2e18;
+
 
 signed main()
 {
-	int n;
-	cin>>n;
-
-	int dp[26][n];
+    
 	
-	return 0;
+    return 0;
 }
