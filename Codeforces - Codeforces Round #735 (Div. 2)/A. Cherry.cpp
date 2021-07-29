7e6d5434a6e8a20748a75a1a22d183ec8e7efecb
Aryan Agarwal <aryan.cs.kgp@gmail.com>
2021-07-29 23:22:07 +0530

    2021-07-29 23:22:07

---
+++ Codeforces - Codeforces Round #735 (Div. 2)/A. Cherry.cpp
@@ -0,0 +1,45 @@
+/*
+	group : Codeforces - Codeforces Round #735 (Div. 2)
+	name : A. Cherry.cpp
+	srcPath : /home/aryan/Desktop/cp-workspace/A_Cherry.cpp
+	url : https://codeforces.com/contest/1554/problem/0
+*/
+/**
+ *    author:  Aryan Agarwal
+ *    created: 29.07.2021 20:07:37 IST
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve()
+{
+    int n;
+    cin>>n;
+
+    vector<int> a(n);
+    int ans=0;
+
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+        if(i)
+        {
+            ans=max(ans,a[i]*a[i-1]);
+        }
+    }
+
+    cout<<ans<<"\n";
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _t = 1;
+    cin>>_t;
+    while(_t--)solve();
+    return 0;
+}
+//	parsed : 29-July-2021 20:05:14 IST
