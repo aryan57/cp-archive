77b54e96b5d52a43b35479de1cb362824b3197b8
Aryan Agarwal <aryan.cs.kgp@gmail.com>
2021-08-02 21:21:42 +0530

    2021-08-02 21:21:42

--- local/c.cpp
+++ local/c.cpp
@@ -6,55 +6,16 @@
 */
 /**
  *    author:  Aryan Agarwal
- *    created: 31.07.2021 23:36:16 IST
+ *    created: 02.08.2021 21:11:58 IST
 **/
 #include <bits/stdc++.h>
 using namespace std;
 
 #define int long long
 
-bool is_compact(int x,int y,int n)
-{
-    while(n)
-    {
-        if(n%10!=x && n%10!=y)
-        {
-            return false;
-        }
-        n/=10;
-    }
-    return true;
-}
-
 void solve()
 {
-    int M=1e9+7;
-    int x,y,n;
-    cin>>x>>y>>n;
-    vector<vector<int>> dp(n+1,vector<int>(9*n+1));
-    dp[0][0]=1;
-    for(int i=1;i<=n;i++)
-    {
-        for(int d=(i==n?1:0);d<=9;d++)
-        {
-            for(int j=d;j<=9*n;j++)
-            {
-                dp[i][j]+=dp[i-1][j-d];
-                dp[i][j]%=M;
-            }
-        }
-    }
-
-    int ans=0;
-    for(int j=1;j<=9*n;j++)
-    {
-        if(is_compact(x,y,j))
-        {
-            ans+=dp[n][j];
-            ans%=M;
-        }
-    }
-    cout<<ans<<"\n";
+    vector<int> v(4);
 }
 
 signed main()
