509b93fe676d6aa6139c69822b663236ae3c30af
Aryan Agarwal <aryan.cs.kgp@gmail.com>
2021-08-04 18:12:25 +0530

    2021-08-04 18:12:25

--- Codeforces - Codeforces Round #425 (Div. 2)/D. Misha, Grisha and Underground.cpp
+++ Codeforces - Codeforces Round #425 (Div. 2)/D. Misha, Grisha and Underground.cpp
@@ -6,62 +6,84 @@
 */
 /**
  *    author:  Aryan Agarwal
- *    created: 03.08.2021 13:49:27 IST
+ *    created: 04.08.2021 17:27:13 IST
 **/
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int long long
+// #define int long long
 const int mxn=1e5;
-vector<int> adj[mxn+1];
-int T=0;
-vector<int> tin(mxn+1),tout(mxn+1),depth(mxn+1);
 const int mxbit=17;
 int par[mxn+1][mxbit+1];
+vector<int> adj[mxn+1],tin(mxn+1),tout(mxn+1),depth(mxn+1);
+int T=0;
 
-void dfs(int u,int p=0,int d=0){
+void dfs(int u,int p,int d){
 	tin[u]=++T;
 	depth[u]=d;
 	par[u][0]=p;
-	for(int v : adj[u]){
-		if(v!=p)dfs(v,u,d+1);
+	for(int to : adj[u]){
+		if(to!=p)dfs(to,u,d+1);
 	}
 	tout[u]=++T;
 }
 
-bool is_ancestor(int v,int u){
-	return tin[v]<=tin[u] && tout[u]<=tout[v];
+void preprocess(int n){
+	for(int bit=1;bit<=mxbit;bit++){
+		for(int i=1;i<=n;i++){
+			if(par[i][bit-1]!=0){
+				par[i][bit]=par[par[i][bit-1]][bit-1];
+			}
+		}
+	}
 }
 
+bool is_ancestor(int a,int b){
+	return tin[a]<=tin[b] && tout[b]<=tout[a];
+}
+
+// lca of node 'a' and 'b' considering '1' as root
 int lca(int a,int b){
 	if(is_ancestor(a,b))return a;
 	if(is_ancestor(b,a))return b;
 
-	for(int bit=0;bit<=mxbit;bit++){
-		if(is_ancestor(par[a][bit],b)){
-			assert(is_ancestor(par[a][bit],a));
-			if(bit==0)return par[a][bit];
-			return lca(par[a][bit-1],b);
+	for(int bit=mxbit;bit>=0;bit--){
+		if(!is_ancestor(par[a][bit],b)){
+			a=par[a][bit];
 		}
 	}
+	
+	return par[a][0];
+}
+
+int dist(int a,int b){
+	int l=lca(a,b);
+	return depth[a]+depth[b]-2*depth[l];
+}
+
+// lca of node 'a' and 'b' considering 'r' as root
+int lca(int a,int b,int r){
+	int x=lca(a,b);
+	int y=lca(a,r);
+	int z=lca(b,r);
+
+	/*
+		atleast two of the lca's in 3c2 pairs, must be equal,
+		https://stackoverflow.com/questions/25371865/find-multiple-lcas-in-unrooted-tree
+	*/
+
+	if(x==y)return z;
+	if(x==z)return y;
+	if(y==z)return x;
 
 	assert(false);
 	return -1;
 }
 
-void fun(int a,int b,int c,int &ans)
-{
-	int z=lca(a,b);
+int cal(int u,int v,int r){
 
-	if(is_ancestor(z,c) && is_ancestor(c,a)){
-
-		int dac=depth[a]-depth[c]+1;
-		int dcb=depth[b]+depth[c]-2*depth[z]+1;
-
-		ans=max(ans,dac);
-		ans=max(ans,dcb);
-
-	}
+	int t=lca(u,v,r);
+	return dist(t,r);
 }
 
 void solve()
@@ -72,42 +94,22 @@ void solve()
 	for(int i=2;i<=n;i++){
 		int p;
 		cin>>p;
-		adj[p].push_back(i);
 		adj[i].push_back(p);
+		adj[p].push_back(i);
 	}
-
-	dfs(1);
-
-	for(int bit=1;bit<=mxbit;bit++){
-		for(int i=1;i<=n;i++){
-			if(par[i][bit-1]){
-				par[i][bit]=par[par[i][bit-1]][bit-1];
-			}
-		}
-	}
-
+	adj[0].push_back(1);
+	dfs(0,0,0);
+	preprocess(n);
 	while(q--){
-		vector<int> v(3);
-		cin>>v[0]>>v[1]>>v[2];
+		int a,b,c;
+		cin>>a>>b>>c;
 
 		int ans=0;
+		ans=max(ans,cal(a,b,c));
+		ans=max(ans,cal(a,c,b));
+		ans=max(ans,cal(c,b,a));
 
-		do{
-			int a=v[0];
-			int b=v[1];
-			int c=v[2];
-
-			// if 'c' appears in path from 'a' to 'b'
-			// ans will be max of dist(a,c) and dist(c,b)
-
-			fun(a,b,c,ans);
-			swap(a,b);
-			fun(a,b,c,ans);
-
-		}while(next_permutation(v.begin(),v.end()));
-
-		cout<<ans<<"\n";
-
+		cout<<ans+1<<"\n";
 	}
 
 }
@@ -121,4 +123,4 @@ signed main()
 	while(_t--)solve();
 	return 0;
 }
-//	parsed : 03-August-2021 13:49:18 IST
+//	parsed : 04-August-2021 17:26:50 IST
