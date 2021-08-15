f097865b027eaabc9eb8cff1e52d72e5c63edd49
Aryan Agarwal <aryan.cs.kgp@gmail.com>
2021-08-15 22:34:10 +0530

    2021-08-15 22:34:10

---
+++ Codeforces - Codeforces Round #738 (Div. 2)/D1. Mocha and Diana (Easy Version).cpp
@@ -0,0 +1,154 @@
+/*
+	group : Codeforces - Codeforces Round #738 (Div. 2)
+	name : D1. Mocha and Diana (Easy Version).cpp
+	srcPath : /home/aryan/Desktop/cp-workspace/D1_Mocha_and_Diana_Easy_Version_.cpp
+	url : https://codeforces.com/contest/1559/problem/D1
+*/
+/**
+ *    author:  Aryan Agarwal
+ *    created: 15.08.2021 20:37:07 IST
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+/* 
+	dsu d(int n)
+	// It creates an undirected graph with n vertices and 0 edges.
+	// 0<=n<=1e8
+	// O(n)
+
+	int d.merge(int a, int b)
+	// It adds an edge (a, b)
+	// If the vertices a and b were in the same connected component, it returns the representative of this connected component. Otherwise, it returns the representative of the new connected component.
+	// 0<=a<n   0<=b<n
+	// O(alpha(n)) amortized
+
+	bool d.same(int a, int b)
+	// Returns whether the vertices a and b are in the same connected component
+	// 0<=a<n   0<=b<n
+	// O(alpha(n)) amortized
+
+	int d.leader(int a)
+	// Returns the representative of the connected component that contains the vertex a
+	// 0<=a<n
+	// O(alpha(n)) amortized
+
+	int d.size(int a)
+	// Returns the size of the connected component that contains the vertex a.
+	// 0<=a<n
+	// O(alpha(n)) amortized
+
+	vector<vector<int>> d.groups()
+	// Returns the list of the "list of the vertices in a connected component"
+	// O(n)
+*/
+struct dsu {
+  public:
+	dsu() : _n(0) {}
+	dsu(int n) : _n(n), parent_or_size(n, -1) {}
+
+	int merge(int a, int b) {
+		assert(0 <= a && a < _n);
+		assert(0 <= b && b < _n);
+		int x = leader(a), y = leader(b);
+		if (x == y) return x;
+		if (-parent_or_size[x] < -parent_or_size[y]) std::swap(x, y);
+		parent_or_size[x] += parent_or_size[y];
+		parent_or_size[y] = x;
+		return x;
+	}
+
+	bool same(int a, int b) {
+		assert(0 <= a && a < _n);
+		assert(0 <= b && b < _n);
+		return leader(a) == leader(b);
+	}
+
+	int leader(int a) {
+		assert(0 <= a && a < _n);
+		if (parent_or_size[a] < 0) return a;
+		return parent_or_size[a] = leader(parent_or_size[a]);
+	}
+
+	int size(int a) {
+		assert(0 <= a && a < _n);
+		return -parent_or_size[leader(a)];
+	}
+
+	std::vector<std::vector<int>> groups() {
+		std::vector<int> leader_buf(_n), group_size(_n);
+		for (int i = 0; i < _n; i++) {
+			leader_buf[i] = leader(i);
+			group_size[leader_buf[i]]++;
+		}
+		std::vector<std::vector<int>> result(_n);
+		for (int i = 0; i < _n; i++) {
+			result[i].reserve(group_size[i]);
+		}
+		for (int i = 0; i < _n; i++) {
+			result[leader_buf[i]].push_back(i);
+		}
+		result.erase(
+			std::remove_if(result.begin(), result.end(),
+						   [&](const std::vector<int>& v) { return v.empty(); }),
+			result.end());
+		return result;
+	}
+
+  private:
+	int _n;
+	// root node: -1 * component size
+	// otherwise: parent
+	std::vector<int> parent_or_size;
+};
+
+void solve()
+{
+	int n,m1,m2;
+	cin>>n>>m1>>m2;
+
+	dsu d1(n),d2(n);
+
+	for(int i=0;i<m1;i++){
+		int u,v;
+		cin>>u>>v;
+		d1.merge(u-1,v-1);
+	}
+	for(int i=0;i<m2;i++){
+		int u,v;
+		cin>>u>>v;
+		d2.merge(u-1,v-1);
+	}
+
+	vector<pair<int,int>> ans;
+
+	for(int i=0;i<n;i++){
+		for(int j=i+1;j<n;j++){
+
+			if(!d1.same(i,j) && !d2.same(i,j)){
+				ans.push_back({i+1,j+1});
+				d1.merge(i,j);
+				d2.merge(i,j);
+			}
+
+		}
+	}
+
+	cout<<ans.size()<<"\n";
+	for(auto p : ans){
+		cout<<p.first<<" "<<p.second<<"\n";
+	}
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int _t = 1;
+	// cin>>_t;
+	while(_t--)solve();
+	return 0;
+}
+//	parsed : 15-August-2021 20:35:18 IST
