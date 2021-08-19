a863d4b23f32e4249bb6b041950b6953040daaaf
Aryan Agarwal <aryan.cs.kgp@gmail.com>
2021-08-19 20:30:23 +0530

    2021-08-19 20:30:23

--- local/local/c.cpp
+++ local/local/c.cpp
@@ -6,24 +6,79 @@
 */
 /**
  *    author:  Aryan Agarwal
- *    created: 18.08.2021 13:17:12 IST
+ *    created: 19.08.2021 12:26:59 IST
 **/
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int long long
+struct Node{
+	int val;
+	Node* next;
 
-void solve()
-{
-	
-}
+	Node(){
+		val=0;
+		next=NULL;
+	}
+	Node(int x){
+		val=x;
+		next=NULL;
+	}
+};
+
+
 
 signed main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	int _t = 1;
-	// cin>>_t;
-	while(_t--)solve();
+
+	/*
+	*/
+
+	Node* head = NULL;
+
+	cout<<head->val<<"\n";
+
+	// 1,2,3,4,5,6
+
+
+	Node* head = new Node(1);
+	Node* temp = head;
+
+
+	for(int i=2;i<=5;i++){
+		temp->next = new Node(i);
+		temp=temp->next;
+	}
+
+	int n=0;
+	temp=head;
+	while(temp!=NULL){
+		n++;
+		temp=temp->next;
+	}
+
+	/*
+		falsy value -> 0,NULL,nullptr
+		truly value ->
+	*/
+
+	if(n==0){
+		cout<<"-1\n";
+		return 0;
+	}
+
+	int k = n/2+1;
+
+	int cur=0;
+	temp=head;
+	while(temp!=NULL){
+		cur++;
+		if(cur==k){
+			cout<<temp->val<<"\n";
+			break;
+		}
+
+		temp=temp->next;
+	}
+
 	return 0;
 }
