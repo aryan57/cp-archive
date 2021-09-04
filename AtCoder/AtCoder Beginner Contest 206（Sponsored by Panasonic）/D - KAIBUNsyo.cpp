1923fd54d4e5be781cfb7cc6eb21beb40691f577
Aryan Agarwal <aryan.cs.kgp@gmail.com>
2021-09-04 12:14:50 +0530

    2021-09-04 12:14:50

--- AtCoder/AtCoder Beginner Contest 206（Sponsored by Panasonic）/D - KAIBUNsyo.cpp
+++ AtCoder/AtCoder Beginner Contest 206（Sponsored by Panasonic）/D - KAIBUNsyo.cpp
@@ -1,115 +1,170 @@
 /*
 	group : AtCoder - AtCoder Beginner Contest 206（Sponsored by Panasonic）
 	name : D - KAIBUNsyo.cpp
-	srcPath : /home/aryan/fdsms/D_KAIBUNsyo.cpp
+	srcPath : /home/aryan/Desktop/cp-workspace/D_KAIBUNsyo.cpp
 	url : https://atcoder.jp/contests/abc206/tasks/abc206_d
 */
-/*
-    author : aryan57
-    created : 19-June-2021 17:40:07 IST
-*/
+/**
+ *    author:  Aryan Agarwal
+ *    created: 04.09.2021 11:41:03 IST
+**/
 #include <bits/stdc++.h>
 using namespace std;
 
 template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
 template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-void dbg_out() { cerr << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
-#ifndef ONLINE_JUDGE
-#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+// #ifndef ONLINE_JUDGE
+#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+// #else
+// #define dbg(...)
+// #endif
 
-#define int long long
-#define X first
-#define Y second
-#define pb push_back
-#define sz(a) ((int)(a).size())
-#define all(a) (a).begin(), (a).end()
-#define F(i, a, b) for (int i = a; i <= b; i++)
-#define RF(i, a, b) for (int i = a; i >= b; i--)
+/* 
+	dsu d(int n)
+	// It creates an undirected graph with n vertices and 0 edges.
+	// 0<=n<=1e8
+	// O(n)
 
+	int d.merge(int a, int b)
+	// It adds an edge (a, b)
+	// If the vertices a and b were in the same connected component, it returns the representative of this connected component. Otherwise, it returns the representative of the new connected component.
+	// 0<=a<n   0<=b<n
+	// O(alpha(n)) amortized
+
+	bool d.same(int a, int b)
+	// Returns whether the vertices a and b are in the same connected component
+	// 0<=a<n   0<=b<n
+	// O(alpha(n)) amortized
+
+	int d.leader(int a)
+	// Returns the representative of the connected component that contains the vertex a
+	// 0<=a<n
+	// O(alpha(n)) amortized
+
+	int d.size(int a)
+	// Returns the size of the connected component that contains the vertex a.
+	// 0<=a<n
+	// O(alpha(n)) amortized
+
+	vector<vector<int>> d.groups()
+	// Returns the list of the "list of the vertices in a connected component"
+	// O(n)
+*/
+struct dsu {
+  public:
+	dsu() : _n(0) {}
+	dsu(int n) : _n(n), parent_or_size(n, -1) {}
+
+	int merge(int a, int b) {
+		assert(0 <= a && a < _n);
+		assert(0 <= b && b < _n);
+		int x = leader(a), y = leader(b);
+		if (x == y) return x;
+		if (-parent_or_size[x] < -parent_or_size[y]) std::swap(x, y);
+		parent_or_size[x] += parent_or_size[y];
+		parent_or_size[y] = x;
+		return x;
+	}
+
+	bool same(int a, int b) {
+		assert(0 <= a && a < _n);
+		assert(0 <= b && b < _n);
+		return leader(a) == leader(b);
+	}
+
+	int leader(int a) {
+		assert(0 <= a && a < _n);
+		if (parent_or_size[a] < 0) return a;
+		return parent_or_size[a] = leader(parent_or_size[a]);
+	}
+
+	int size(int a) {
+		assert(0 <= a && a < _n);
+		return -parent_or_size[leader(a)];
+	}
+
+	std::vector<std::vector<int>> groups() {
+		std::vector<int> leader_buf(_n), group_size(_n);
+		for (int i = 0; i < _n; i++) {
+			leader_buf[i] = leader(i);
+			group_size[leader_buf[i]]++;
+		}
+		std::vector<std::vector<int>> result(_n);
+		for (int i = 0; i < _n; i++) {
+			result[i].reserve(group_size[i]);
+		}
+		for (int i = 0; i < _n; i++) {
+			result[leader_buf[i]].push_back(i);
+		}
+		result.erase(
+			std::remove_if(result.begin(), result.end(),
+						   [&](const std::vector<int>& v) { return v.empty(); }),
+			result.end());
+		return result;
+	}
+
+  private:
+	int _n;
+	// root node: -1 * component size
+	// otherwise: parent
+	std::vector<int> parent_or_size;
+};
+
+
+// #define int long long
 const int mxn = 2e5;
-const long long INF = 2e18;
-const int32_t M = 1000000007;
-// const int32_t M = 998244353;
-const long double pie = acos(-1);
+vector<int> pos(mxn,-1);
 
-void solve_LOG()
+void solve()
 {
-    int n;
-    cin>>n;
+	int n;
+	cin>>n;
+	dsu d(n);
+	vector<int>a(n);
+	for(int i=0;i<n;i++){
+		int x;
+		cin>>x;
+		x--;
+		a[i]=x;
+		if(pos[x]!=-1){
+			d.merge(pos[x],i);
+		}
+		pos[x]= i;
+	}
 
-    vector <int> a(n);
+	int l=0;
+	int r=n-1;
+	while(l<r){
+		d.merge(l,r);
+		l++;
+		r--;
+	}
 
-    vector<set <int>> pos(mxn+1);
+	vector<vector<int>> groups = d.groups();
+	// dbg(groups);
+	int ans=0;
 
-    F(i,0,n-1)
-    {
-        cin>>a[i];
-        pos[a[i]].insert(i);
-    }
+	for(auto v : groups){
+		// dbg(v);
+		set<int> s;
+		for(int x : v){
+			s.insert(a[x]);
+		}
+		ans+=s.size() - 1;
+	}
 
-    int p=0;
-    int q=n-1;
-    int cnt=0;
-    while (p<q)
-    {
-        if(a[p]==a[q])
-        {
-            p++;
-            q--;
-            continue;
-        }
-
-        int x=a[p];
-        int y=a[q];
-
-        if(sz(pos[a[p]])>sz(pos[a[q]]))swap(x,y);
-
-        for(auto it =pos[x].begin();it!=pos[x].end();it++)
-        {
-            a[*it]=y;
-            pos[y].insert(*it);
-        }
-
-
-        p++;
-        q--;
-        cnt++;
-    }
-
-    cout<<cnt;
-    
+	cout<<ans;
 }
 
 signed main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-#ifndef ONLINE_JUDGE
-// freopen("input.txt","r",stdin);
-// freopen("output.txt","w",stdout);
-#endif
-#ifdef ARYAN_SIEVE
-    sieve();
-#endif
-#ifdef ARYAN_SEG_SIEVE
-    segmented_sieve();
-#endif
-#ifdef ARYAN_FACT
-    fact_init();
-#endif
-    // cout<<fixed<<setprecision(10);
-    int _t=1;
-    // cin>>_t;
-    for (int i=1;i<=_t;i++)
-    {
-        // cout<<"Case #"<<i<<": ";
-        solve_LOG();
-    }
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int _t = 1;
+	// cin>>_t;
+	while(_t--)solve();
+	return 0;
 }
-//	parsed : 19-June-2021 17:39:50 IST
+//	parsed : 04-September-2021 11:40:58 IST
