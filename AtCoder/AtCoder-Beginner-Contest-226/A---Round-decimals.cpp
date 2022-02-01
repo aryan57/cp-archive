ba3142d45c40e3631248d267a2c8fc602d4b5699
aryan57 <aryan.cs.kgp@gmail.com>
2022-02-02 04:36:10 +0530

    2022-02-02 04:36:10

---
+++ AtCoder/AtCoder-Beginner-Contest-226/A---Round-decimals.cpp
@@ -0,0 +1,56 @@
+/*
+	group : AtCoder - AtCoder Beginner Contest 226
+	name : A - Round decimals.cpp
+	srcPath : /home/aryan/Desktop/A_Round_decimals.cpp
+	url : https://atcoder.jp/contests/abc226/tasks/abc226_a
+*/
+/*
+	group : AtCoder - AtCoder Beginner Contest 226
+	name : A - Round decimals.cpp
+	srcPath : /home/aryan/Desktop/cp-workspace/A_Round_decimals.cpp
+	url : https://atcoder.jp/contests/abc226/tasks/abc226_a
+*/
+/**
+ *    author:  Aryan Agarwal
+ *    created: 07.11.2021 17:30:41 IST
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve()
+{
+	string x;
+	cin>>x;
+
+	int ind=-1;
+	int n=x.size();
+	int zz=0;
+	for(int i=0;i<n;i++){
+
+		if(x[i]=='.'){
+			if(i<n-1 && x[i+1]>='5'){
+				zz++;
+			}
+			break;
+		}else{
+			zz*=10;
+			zz+=x[i]-'0';
+		}
+	}
+
+	cout<<zz;
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int _t = 1;
+	// cin>>_t;
+	while(_t--)solve();
+	return 0;
+}
+//	parsed : 07-November-2021 17:30:12 IST
+//	parsed : 02-February-2022 04:35:51 IST
