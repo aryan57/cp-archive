cfb953278e9e72bc849ccc583a049ad7c72d8d7b
Aryan Agarwal <55305876+aryan57@users.noreply.github.com>
2022-06-03 03:09:56 +0530

    2022-06-03 03:09:56

---
+++ Codeforces-auto-uploads/Codeforces_Round_755_Div_2_based_on_Technocup_2022_Elimination_Round_2/B_-_Coloring_Rectangles.cpp
@@ -0,0 +1,44 @@
+/**
+ *    author:  Aryan Agarwal
+ *    created: 14.11.2021 11:45:25 IST
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+int f(int n,int m)
+{
+	int ans = (n/3 + n%3)*m;
+	int z=(n/3)*m + (n%3)*(m/3);
+	if(n%3 || m%3){
+		z++;
+		if(n%3==2 && m%3==2)z++;
+
+	}
+	ans=min(ans,z);
+	return ans;
+}
+
+void solve()
+{
+	int n,m;
+	cin>>n>>m;
+
+	int ans=f(n,m);
+	ans=min(ans,f(m,n));
+
+	cout<<ans;
+	cout<<"\n";
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int _t = 1;
+	cin>>_t;
+	while(_t--)solve();
+	return 0;
+}
+//	parsed : 14-November-2021 11:45:23 IST
