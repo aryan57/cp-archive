cde60c7165509039e6af18b64007eb195a448239
Aryan Agarwal <55305876+aryan57@users.noreply.github.com>
2022-06-03 03:09:20 +0530

    2022-06-03 03:09:20

---
+++ Codeforces-auto-uploads/Codeforces_Round_761_Div_2/A_-_Forbidden_Subsequence.cpp
@@ -0,0 +1,61 @@
+/**
+ *    author:  Aryan Agarwal
+ *    created: 16.12.2021 19:07:39 IST
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve()
+{
+	string s,t;
+	cin>>s>>t;
+
+	string ss=s;
+	sort(ss.begin(),ss.end());
+	int cnt[3]={};
+	int cnt2[3]={};
+	string zz="";
+	for(char x : ss){
+		if(x<='c'){
+			cnt[x-'a']=1;
+			cnt2[x-'a']++;
+		}else{
+			zz+=x;
+		}
+	}
+
+	if(cnt[0]+cnt[2]+cnt[1]!=3 || t!="abc"){
+		cout<<ss;
+		cout<<"\n";
+		return;
+	}
+
+	while (cnt2[0]--)
+	{
+		cout<<'a';
+	}
+	while (cnt2[2]--)
+	{
+		cout<<'c';
+	}
+	while (cnt2[1]--)
+	{
+		cout<<'b';
+	}
+
+	cout<<zz;
+	cout<<"\n";
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int _t = 1;
+	cin>>_t;
+	while(_t--)solve();
+	return 0;
+}
+//	parsed : 16-December-2021 19:05:08 IST
