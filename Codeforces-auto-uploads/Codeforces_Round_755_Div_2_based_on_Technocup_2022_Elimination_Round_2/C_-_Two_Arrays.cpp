ef43711d215aeeac932c375801093c8968a63987
Aryan Agarwal <55305876+aryan57@users.noreply.github.com>
2022-06-03 03:09:55 +0530

    2022-06-03 03:09:55

---
+++ Codeforces-auto-uploads/Codeforces_Round_755_Div_2_based_on_Technocup_2022_Elimination_Round_2/C_-_Two_Arrays.cpp
@@ -0,0 +1,54 @@
+/**
+ *    author:  Aryan Agarwal
+ *    created: 14.11.2021 11:54:15 IST
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve()
+{
+	int n;
+	cin>>n;
+	priority_queue<int> a,b;
+	for(int i=0;i<n;i++){
+		int x;
+		cin>>x;
+		a.push(x);
+	}
+	for(int i=0;i<n;i++){
+		int x;
+		cin>>x;
+		b.push(x);
+	}
+
+	while (!a.empty() && !b.empty())
+	{
+		int xa = a.top();
+		a.pop();
+		int xb = b.top();
+		b.pop();
+
+		if(xa==xb || xa+1==xb){
+			continue;
+		}
+		cout<<"NO\n";
+		return;
+		
+	}
+
+	cout<<"YES\n";
+	
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int _t = 1;
+	cin>>_t;
+	while(_t--)solve();
+	return 0;
+}
+//	parsed : 14-November-2021 11:48:32 IST
