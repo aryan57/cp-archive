93849865ca47ced33e5b8b2d60086103cb73e22f
Aryan Agarwal <55305876+aryan57@users.noreply.github.com>
2022-06-03 03:10:13 +0530

    2022-06-03 03:10:13

---
+++ Codeforces-auto-uploads/Codeforces_Round_752_Div_2/C_-_Di-visible_Confusion.cpp
@@ -0,0 +1,41 @@
+/**
+ *    author:  Aryan Agarwal
+ *    created: 31.10.2021 09:09:04 IST
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve()
+{
+	int n;
+	cin>>n;
+	bool yes=true;
+	for(int i=1;i<=n;i++){
+		int x;
+		cin>>x;
+		bool ok = false;
+		for(int j=i+1;j>=2;j--){
+			if(x%j){
+				ok=true;
+				break;
+			}
+		}
+		yes&=ok;
+	}
+
+	if(yes)cout<<"YES\n";
+	else cout<<"NO\n";
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int _t = 1;
+	cin>>_t;
+	while(_t--)solve();
+	return 0;
+}
+//	parsed : 31-October-2021 09:08:57 IST
