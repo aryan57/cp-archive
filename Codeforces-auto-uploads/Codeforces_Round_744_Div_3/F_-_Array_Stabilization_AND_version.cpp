7c043be803a56e6081bbdc72a5dfc82ac8ec9ce5
Aryan Agarwal <55305876+aryan57@users.noreply.github.com>
2022-06-03 03:11:07 +0530

    2022-06-03 03:11:07

---
+++ Codeforces-auto-uploads/Codeforces_Round_744_Div_3/F_-_Array_Stabilization_AND_version.cpp
@@ -0,0 +1,69 @@
+/**
+ *    author:  Aryan Agarwal
+ *    created: 28.09.2021 21:56:43 IST
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve()
+{
+	int n,d;
+	cin>>n>>d;
+
+	vector <int> v(n);
+	vector< vector<int> > adj(n);
+	queue<int> q;
+	vector<bool> visited(n);
+	vector<int> distance(n);
+	for(int i=0;i<n;i++){
+		cin>>v[i];
+		int tt=(i+n-d+n)%n;
+		adj[tt].push_back(i);
+		if(v[i]==0){
+			q.push(i);
+			visited[i]=true;
+		}
+	}
+
+	
+	while (!q.empty()) {
+	
+		int parent_node = q.front();
+		q.pop();
+	
+		for (int child_node : adj[parent_node]) {
+	
+			if (!visited[child_node])
+			{
+				visited[child_node] = true;
+				q.push(child_node);
+				distance[child_node] = distance[parent_node] + 1;
+			}
+		}
+	}
+	int ans=0;
+	for(int i=0;i<n;i++){
+		if(!visited[i]){
+			cout<<"-1\n";
+			return;
+		}
+
+		ans=max(ans,distance[i]);
+	}
+
+	cout<<ans;
+	cout<<"\n";
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int _t = 1;
+	cin>>_t;
+	while(_t--)solve();
+	return 0;
+}
+//	parsed : 28-September-2021 21:56:38 IST
