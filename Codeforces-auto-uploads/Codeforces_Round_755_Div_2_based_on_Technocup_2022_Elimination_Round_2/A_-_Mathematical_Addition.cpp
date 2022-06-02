a87828b46d35ab01fc39e4ad0e853542a8e3ccaf
Aryan Agarwal <55305876+aryan57@users.noreply.github.com>
2022-06-03 03:09:57 +0530

    2022-06-03 03:09:56

---
+++ Codeforces-auto-uploads/Codeforces_Round_755_Div_2_based_on_Technocup_2022_Elimination_Round_2/A_-_Mathematical_Addition.cpp
@@ -0,0 +1,28 @@
+/**
+ *    author:  Aryan Agarwal
+ *    created: 14.11.2021 11:39:00 IST
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve()
+{
+	int u,v;
+	cin>>u>>v;
+
+	cout<<u*u<<" "<<-v*v;
+	cout<<"\n";
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int _t = 1;
+	cin>>_t;
+	while(_t--)solve();
+	return 0;
+}
+//	parsed : 14-November-2021 11:35:34 IST
