b03012e8cce3ec95d3e58830a9931e9b7f5f2832
Aryan Agarwal <55305876+aryan57@users.noreply.github.com>
2022-06-03 03:11:27 +0530

    2022-06-03 03:11:26

---
+++ Codeforces-auto-uploads/Educational_Codeforces_Round_113_Rated_for_Div_2/B_-_Chess_Tournament.cpp
@@ -0,0 +1,75 @@
+/**
+ *    author:  Aryan Agarwal
+ *    created: 08.09.2021 20:33:55 IST
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve()
+{
+	int n;
+	cin>>n;
+	string s;
+	cin>>s;
+
+	int c2=0;
+
+	for(char x : s)
+	{
+		if(x=='2')c2++;
+	}
+
+	if(c2==1 || c2==2){
+		cout<<"NO\n";
+		return;
+	}
+
+	s='#'+s;
+	vector<vector<char>> g(n+1,vector<char>(n+1,'X'));
+	vector<int> p;
+	for(int i=1;i<=n;i++)
+	{
+		if(s[i]=='2')
+		{
+			p.push_back(i);
+		}
+		for(int j=1;j<=n;j++)
+		{
+			if(i==j)continue;
+			g[i][j]='=';
+		}
+	}
+
+	if(!p.empty())
+	{
+		assert(p.size()>=3);
+
+		for(int i=0;i<p.size();i++)
+		{
+			g[p[i]][p[(i+1)%(p.size())]]='+';
+			g[p[(i+1)%(p.size())]][p[i]]='-';
+		}
+	}
+	cout<<"YES\n";
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=n;j++)
+		{
+			cout<<g[i][j];
+		}cout<<"\n";
+	}
+
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int _t = 1;
+	cin>>_t;
+	while(_t--)solve();
+	return 0;
+}
+//	parsed : 08-September-2021 20:27:29 IST
