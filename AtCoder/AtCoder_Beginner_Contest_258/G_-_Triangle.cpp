c27275de8f78608de4c591e8c9dcf1a655f0381d
Aryan Agarwal <aryan.cs.kgp@gmail.com>
2022-07-02 21:27:03 +0530

    2022-07-02 21:27:03

--- AtCoder/AtCoder_Beginner_Contest_258/G_-_Triangle.cpp
+++ AtCoder/AtCoder_Beginner_Contest_258/G_-_Triangle.cpp
@@ -1,59 +1,33 @@
 /**
  *    author:  Aryan Agarwal
- *    created: 02.07.2022 18:54:39 IST
+ *    created: 02.07.2022 21:24:26 IST
 **/
 #include <bits/stdc++.h>
 using namespace std;
 
 #define int long long
 
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-void dbg_out() { cerr << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
-#ifndef ONLINE_JUDGE
-#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
-
 void solve()
 {
 	int n;
 	cin>>n;
+	vector <bitset<3000>> v(n);
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			char c;
+			cin>>c;
+			v[i][j]=c-'0';
+		}
+	}
 
-	vector <string> g(n);
-	for(auto &s : g)cin>>s;
-
-	vector< vector<int> > adj(n);
-
+	int cnt=0;
 	for(int i=0;i<n;i++){
 		for(int j=i+1;j<n;j++){
-			if(g[i][j]=='0')continue;
-			adj[i].push_back(j);
-			adj[j].push_back(i);
+			if(!v[i][j])continue;
+			cnt+=(v[i]&v[j]).count();
 		}
 	}
-
-	int tot=0;
-	set<vector <int> > s;
-
-	for(int i=0;i<n;i++){
-		int k=adj[i].size();
-		for(int t1=0;t1<k;t1++){
-			for(int t2=t1+1;t2<k;t2++){
-				int u = adj[i][t1];
-				int v = adj[i][t2];
-				if(g[u][v]=='1'){
-					vector <int> temp = {i,u,v};
-					sort(temp.begin(),temp.end());
-					s.insert(temp);
-				}
-			}
-		}
-	}
-
-	cout<<s.size();
+	cout<<cnt/3;
 }
 
 signed main()
